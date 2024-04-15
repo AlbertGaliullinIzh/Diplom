ce34dbb08d5ae78905dcc9966cd5c2c3a368cd3f
Albert PC ILINA <root@vkfdkv.myguest.virtualbox.org>
2024-04-15 10:25:52 +0400

    scryp automatic installing zabbix with configuration

---
+++ ZabbixInstallAndConfig.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+SI=""
+
+apt-get install update
+apt-get install zabbix-agent -y
+
+sed -i "s/Server=127.0.0.1/Server=$SI/g" /etc/zabbix/zabbix_agentd.conf
+sed -i "s/ServerActive=127.0.0.1/ServerActive=$SI/g" /etc/zabbix/zabbix_agentd.conf
+sed -i "s/ServerActive=/ServerActive=$SI/g" /etc/zabbix/zabbix_agentd.conf
+sed -i "s/Server=/Server=$SI/g" /etc/zabbix/zabbix_agentd.conf
+
+systemctl restart zabbix-agent
