eab017ad9f24c989cab800ee5e16b97196d8c105
Albert PC ILINA <root@vkfdkv.myguest.virtualbox.org>
2024-04-27 20:47:09 +0400

    add scrypt for detection scanning ports

---
+++ DetectionScanningPorts.py
@@ -0,0 +1,35 @@
+import json
+import time
+
+machineIp = "192.168.31.83"
+json_list= list()
+
+print("Detection scaning ports start...")
+
+with open('current/conn.log', 'r') as file:
+    current_position = file.tell()
+    while True:
+        file.seek(current_position)
+        json_list += file.readlines()
+        current_position = file.tell()
+        i = 0
+        while i < len(json_list):
+            if type(json_list[i]) == dict:
+                i+=1
+                continue
+            json_list[i] = eval(json_list[i].replace("true","True").replace("false","False"))
+            i+=1
+
+        json_list_for_analiz = [elem for elem in json_list if elem.get("id.orig_h") != elem.get("id.resp_h") and elem.get("id.orig_h") != machineIp]
+        unicationIP = set([elem["id.orig_h"] for elem in json_list])
+
+        for ip in unicationIP:
+            recordsForIp = [elem for elem in json_list_for_analiz if elem.get("id.orig_h") == ip and (elem.get("conn_state") == "S0" or elem.get("conn_state") == "REJ")]
+            if len(recordsForIp) > 10:
+                print(f"Обнаружена подозрительная активность. Предположение: сканирование портов. Адрес: {recordsForIp[0]['id.orig_h']}")
+        
+        if len(json_list) <= 100:
+            pass
+        else:
+            json_list = json_list[-100:]
+        time.sleep(5)
