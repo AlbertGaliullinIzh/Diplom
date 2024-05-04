217a67e0fb59c82ec4df0b2c3c03e2ee438e0baa
AlbertGaliullinIzh <167056403+AlbertGaliullinIzh@users.noreply.github.com>
2024-05-04 12:24:53 +0400

    Update ZabbixInstallServer.sh

--- ZabbixInstallServer.sh
+++ ZabbixInstallServer.sh
@@ -1,29 +1,26 @@
 #!bin/bash
  
-wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-5+debian12_all.deb
-dpkg -i zabbix-release_6.0-5+debian12_all.deb
+wget https://repo.zabbix.com/zabbix/6.4/debian/pool/main/z/zabbix-release/zabbix-release_6.4-1+debian12_all.deb
+dpkg -i zabbix-release_6.4-1+debian12_all.deb
 apt update
+
 
 apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent
 
-
 mysql -uroot -p
 
-create database zabbix character set utf8mb4 collate utf8mb4_bin;
-create user zabbix@localhost identified by 'password';
-grant all privileges on zabbix.* to zabbix@localhost;
-set global log_bin_trust_function_creators = 1;
-quit;
+mysql> create database zabbix character set utf8mb4 collate utf8mb4_bin;
+mysql> create user zabbix@localhost identified by 'password';
+mysql> grant all privileges on zabbix.* to zabbix@localhost;
+mysql> set global log_bin_trust_function_creators = 1;
+mysql> quit;
 
 zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p zabbix
 
-mysql -uroot -p
+# mysql -uroot -p
+password
+mysql> set global log_bin_trust_function_creators = 0;
+mysql> quit;
 
-set global log_bin_trust_function_creators = 0;
-quit;
-
-echo  "DBPassword=P@ssw0rd" >> /etc/zabbix/zabbix_server.conf
-
-
-systemctl restart zabbix-server zabbix-agent apache2
-systemctl enable zabbix-server zabbix-agent apache2
+# systemctl restart zabbix-server zabbix-agent apache2
+# systemctl enable zabbix-server zabbix-agent apache2
