7c931f53d31ce9c24ac00a54c08d2b8108f55ec8
AlbertGaliullinIzh <167056403+AlbertGaliullinIzh@users.noreply.github.com>
2024-05-06 11:08:52 +0400

    Update ZeekInstallAndConfiguration.sh

--- ZeekInstallAndConfiguration.sh
+++ ZeekInstallAndConfiguration.sh
@@ -10,11 +10,14 @@ list_interface=("","")
 
 apt install curl
 
-curl -fsSL https://download.opensuse.org/repositories/security:zeek/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
-echo 'deb http://download.opensuse.org/repositories/security:/zeek/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
+#curl -fsSL https://download.opensuse.org/repositories/security:zeek/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
+#echo 'deb http://download.opensuse.org/repositories/security:/zeek/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
+#apt update
+#apt install zeek-lts -y
 
-apt update
-apt install zeek-lts -y
+
+dpkg -i zeek_6.2.0-0_amd64.deb
+apt-get install -f
 
 echo "export PATH=$PATH:/opt/zeek/bin" >> ~/.bashrc
 source ~/.bashrc
