ebc9d7888b8b9e4db84ebfb2d62df956ae3647bc
AlbertGaliullinIzh <167056403+AlbertGaliullinIzh@users.noreply.github.com>
2024-05-24 14:04:56 +0400

    Update ZeekInstallAndConfiguration.sh

--- ZeekInstallAndConfiguration.sh
+++ ZeekInstallAndConfiguration.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-IP_server=""
+IP_monitoring="IP1"
+list_IP=("IP1" "IP2")
+list_interface=("Int1" "Int2")
 
-IP_monitoring=""
+apt install curl -y
+apt install gpg -y
 
-list_IP=("","")
-list_interface=("","")
-
-
-apt install curl
-
-curl -fsSL https://download.opensuse.org/repositories/security:zeek/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
+curl --proxy http://proxy.istu.ru:8080 -fsSL https://download.opensuse.org/repositories/security:zeek/Debian_12/Release.key |
+gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/security_zeek.gpg > /dev/null
 echo 'deb http://download.opensuse.org/repositories/security:/zeek/Debian_12/ /' | sudo tee /etc/apt/sources.list.d/security:zeek.list
 apt update
 apt install zeek-lts -y
-
-
-#dpkg -i zeek_6.2.0-0_amd64.deb
-#apt-get install -f -y
 
 echo "export PATH=$PATH:/opt/zeek/bin" >> ~/.bashrc
 source ~/.bashrc
@@ -40,16 +34,16 @@ file="/opt/zeek/etc/node.cfg"
 
 cat /dev/null > "$file"
 
-
 echo "[zeek-logger]" >> "$file"
 echo "type=logger" >> "$file"
 echo "host=$IP_monitoring" >> "$file"
-
 echo "" >> "$file"
+
 echo "[zeek-manager]" >> "$file"
 echo "type=manager" >> "$file"
 echo "host=$IP_monitoring" >> "$file"
 echo "" >> "$file"
+
 echo "[zeek-proxy]" >> "$file"
 echo "type=proxy" >> "$file"
 echo "host=$IP_monitoring" >> "$file"
@@ -66,17 +60,19 @@ while [ $counter -lt $list_length ]; do
     ((counter++))
 done
 
+
+
+echo " @load policy/tuning/json-logs.zeek" >> /opt/zeek/share/zeek/site/local.zeek
+./zeekctl check
+./zeekctl deploy
+
+
+
 #echo "" >> "$file"
 #echo "[zeek-worker-lo]" >> "$file"
 #echo "type=worker" >> "$file"
 #echo "host=localhost" >> "$file"
 #echo "interface=lo" >> "$file"
-
-echo " @load policy/tuning/json-logs.zeek" >> /opt/zeek/share/zeek/site/local.zeek
-
-cd /opt/zeek/bin
-./zeekctl check
-./zeekctl deploy
 
 #mkdir /bin/monitoringProccess
 #cd -
