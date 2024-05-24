7e554ad32d6ef5f8bd6ea81c3023b42616043165
AlbertGaliullinIzh <167056403+AlbertGaliullinIzh@users.noreply.github.com>
2024-05-24 11:53:39 +0400

    Update ZabbixInstallAndConfig.sh

--- ZabbixInstallAndConfig.sh
+++ ZabbixInstallAndConfig.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-SI=""
-
-#dpkg -i zabbix-release_6.4-1+debian12_all.deb
-#apt-get install -f -y
+SI="172.31.2.2"
 
 apt-get install update
 apt-get install zabbix-agent -y
-
-#sed -i "s/ServerActive=/ServerActive=$SI/g" /etc/zabbix/zabbix_agentd.conf
-#sed -i "s/Server=/Server=$SI/g" /etc/zabbix/zabbix_agentd.conf
 
 sed -i "s/Server=127.0.0.1/Server=$SI/g" /etc/zabbix/zabbix_agentd.conf
 sed -i "s/ServerActive=127.0.0.1/ServerActive=$SI/g" /etc/zabbix/zabbix_agentd.conf
