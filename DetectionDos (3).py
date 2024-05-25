7ef03e7eb8412690aaac872e2ff9a3e8a452d031
AlbertGaliullinIzh <167056403+AlbertGaliullinIzh@users.noreply.github.com>
2024-05-25 23:37:07 +0400

    Add files via upload

---
+++ DetectionDos (3).py
@@ -0,0 +1,121 @@
+import machine
+import time
+import requests
+import json
+
+class MenegerForDosDetection():
+
+    countIpList = list() # список с количеством Ip
+    countRequestList = list() # список с количеством трафика
+    countControversiallList = list() # список с подозрительным трафиком
+    countMachineKnown = 10 # Количество машин, которые долдны подключится для киберучений
+    
+    machineIp = []
+
+
+    countIpCoef = 0
+    countRequestCoef = 0
+
+    def __init__(self, numberParticipants):
+        self.countMachineKnown = numberParticipants
+        self.machineList = list()
+        self.json_list = list()
+        self.current_position = 0
+
+        self.itMachineIp = ['192.168.31.116']
+
+    def sendmessage(self, ip): # отправка данных о обнаруженном dos
+        print(f"Обнаружена подозрительная активность. Предположение: Dos-аткака. Адрес: {ip}")
+        data = {"name": "VPN", "trigger": "DOS-attack"}
+        json_data = json.dumps(data)
+        url = "http://10.0.2.7:5000"
+        print(data)
+        response = requests.post(url, data=json_data, headers={"Content-Type": "application/json"})
+        if response.status_code == 200:
+            print("Данные успешно отправлены!")
+        else:
+            print(f"Ошибка при отправке данных: {response.status_code} - {response.text}")
+
+    def start(self):
+        while True:
+            self.ReadingFile()
+            unicationIP = set([elem["id.orig_h"] for elem in self.json_list if elem['id.orig_h'] not in self.itMachineIp])
+            for elem in unicationIP:
+                self.UpploadMachine(elem, [item for item in self.json_list if item["id.orig_h"] == elem])
+
+            for elem in self.machineList:
+                #if elem.MachineIsNotActive():
+                #    self.machineList.remove(elem)
+                print(elem.GetIp() + " - " + str(elem.IsMachineAttacking()))
+            print("----")
+            time.sleep(10)
+
+
+
+    def UpploadMachine(self, ip, input_traffic):
+        index = self.GetIdMachineForList(ip)
+        if index == -1:            
+            self.machineList.append(
+                machine.Machine(ip, input_traffic, False if len(self.machineList) < self.countMachineKnown else True))
+        else:
+            self.machineList[index].AddTraffic(input_traffic)
+    
+    def GetIdMachineForList(self,ip):# получение id из списка машин
+        i = 0
+        for elem in self.machineList:
+            if elem.GetIp() == ip:
+                return i
+            i+=1
+        return -1
+    
+    def ReadingFile(self):# считывание с файла, с ранее указаной позиции
+        with open('/opt/zeek/logs/current/conn.log', 'r') as file:
+            file.seek(self.current_position)
+            self.json_list = file.readlines()
+            self.current_position = file.tell()
+            i = 0
+            while i < len(self.json_list):
+                self.json_list[i] = eval(self.json_list[i].replace("true","True").replace("false","False"))
+                i+=1
+
+
+    # #def newIpAddresses():
+    #     def plusCountIpCoef():
+    #         if countIpCoef != 1:
+    #             countIpCoef+=0.1
+    #     def minusCountIpCoef():
+    #         if countIpCoef != 0:
+    #             countIpCoef-=0.1
+    #     def resetCountIpCoef():
+    #         if countIpCoef != 0:
+    #             countIpCoef-=0.1
+    #     unicationIP = set([elem["id.orig_h"] for elem in json_list])
+    #     #добавляем количество ip в список
+    #     if len(countIpList) < 10:
+    #         countIpList.append(len(unicationIP))
+    #     else:
+    #         countIpList.pop(0)
+    #         countIpList.append(len(unicationIP))
+        
+        # # Анализируем количество адресов
+        # # проверка на максимальное количество подлючений
+        # if max(countIpList) == countMachineKnown:
+        #     resetCountIpCoef()
+        #     return
+        #
+        # # если количетсво ip адресов одинакоевое
+        # if len(set(countIpList)) == 1:
+        #     resetCountIpCoef()
+        #     return
+        # # Если количетсво ip адресов варируется
+        # i = 0
+        # while i!=9:
+        #     if countIpList[i] < countIpList[i+1]:
+        #         plusCountIpCoef()
+        #     if countIpList[i] > countIpList[i+1]:
+        #         minusCountIpCoef()
+        # return
+
+manager = MenegerForDosDetection(8)
+manager.start()
+
---
+++ machine (4).py
@@ -0,0 +1,56 @@
+import datetime
+from collections import Counter
+class Machine:
+    def __init__(self, ip, traffic, suspicious):
+        self.ip = ip
+        self.countTraffic = [len(traffic)]
+        self.lastActiveTime = datetime.datetime.now()
+      
+        self.suspiciousTraffic = self.AnalysForSuspiciousTraffic(traffic)
+        self.suspicious = suspicious
+        self.suspiciousTrafficCount = 0
+
+
+    def GetIp(self):
+        return self.ip
+    def GetTraffic(self):
+        return self.traffic
+    def AddTraffic(self, input_traffic): # Добавляяем трафик
+        if len(self.countTraffic) < 10:
+            self.countTraffic.append(len(input_traffic))
+        else:
+             self.countTraffic.pop(0)
+             self.countTraffic.append(len(input_traffic))
+        self.suspiciousTraffic = self.AnalysForSuspiciousTraffic(input_traffic)
+        self.analyzCountTraffic()
+        self.lastActiveTime = datetime.datetime.now()
+    def analyzCountTraffic(self): # анализ на количество трафика
+        i = 0
+        flowGrowth = list()
+        while i!=len(self.countTraffic)-1:
+            flowGrowth.append(self.countTraffic[i+1]/self.countTraffic[i])
+            i+=1
+        grad = (sum(flowGrowth)/len(flowGrowth))/max(flowGrowth)
+        self.suspiciousTrafficCount = grad if grad >=0 else 0
+        return
+    def MachineIsNotActive(self): # если машина неактивка
+        return datetime.datetime.now()-self.lastActiveTime >= datetime.timedelta(minutes=3)
+    def AnalysForSuspiciousTraffic(self,input_traffic):# проверка на подозрительность трафика
+        recordsForIp = [elem for elem in input_traffic if (elem["conn_state"] in ['S0','SHR', 'OTH', 'RSTRH', 'RSTO','REJ'])]
+        return len(recordsForIp) / len(input_traffic)
+        # element_count = Counter([elem['conn_state'] for elem in input_traffic])
+        # print(set(sorted([elem['conn_state'] for elem in input_traffic], key=lambda x: (-element_count[x], x))))
+        # print(set([elem['conn_state'] for elem in input_traffic]))
+        # if len(set([elem['conn_state'] for elem in input_traffic])) >2:
+        #     print("")
+        return 0
+    def IsMachineAttacking(self):
+        res = self.suspiciousTraffic * 0.5 + self.suspicious * 0.1 + self.suspiciousTrafficCount * 0.4
+        if self.suspiciousTraffic * 0.5 + self.suspicious * 0.1 + self.suspiciousTrafficCount * 0.4 > 0.1:
+            print(f"IP - {self.GetIp()} --- {self.suspiciousTraffic * 0.5 + self.suspicious * 0.1 + self.suspiciousTrafficCount * 0.4}")
+            print(f"self.suspiciousTraffic - {self.suspiciousTraffic}    self.suspicious - {self.suspicious}     self.suspiciousTrafficCount - {self.suspiciousTrafficCount}")
+            print("___")
+        self.suspiciousTraffic = 0
+        self.suspicious = 0
+        self.suspiciousTrafficCount = 0
+        return res
