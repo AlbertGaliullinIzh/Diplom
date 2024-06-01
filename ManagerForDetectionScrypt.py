48ad0d5ed27e0fa418ff7c2984a2de3c9d5d6038
AlbertGaliullinIzh <167056403+AlbertGaliullinIzh@users.noreply.github.com>
2024-06-01 11:52:16 +0400

    Update ManagerForDetectionScrypt.py

--- ManagerForDetectionScrypt.py
+++ ManagerForDetectionScrypt.py
@@ -5,16 +5,8 @@ import json
 
 class ManagerForDosDetection():
 
-    #countIpList = list() # список с количеством Ip
-    #countRequestList = list() # список с количеством трафика
-    #countControversiallList = list() # список с подозрительным трафиком
-    countMachineKnown = 10 # Количество машин, которые долдны подключится для киберучений
-    
+    countMachineKnown = 10
     machineIp = []
-
-
-    #countIpCoef = 0
-    #countRequestCoef = 0
 
     def __init__(self, numberParticipants):
         self.countMachineKnown = numberParticipants
@@ -25,46 +17,55 @@ class ManagerForDosDetection():
         self.itMachineIp = ['192.168.31.116']
 
     def sendmessage(self, ip, assumption):
-        print(f"Обнаружена подозрительная активность. Предположение: {assumption}. Адрес: {ip}")
-        data = {"name": "VPN", "trigger": "DOS-attack"}
-        json_data = json.dumps(data)
-        url = "http://10.0.2.7:5000"
-        print(data)
-        response = requests.post(url, data=json_data, headers={"Content-Type": "application/json"})
-        if response.status_code == 200:
-            print("Данные успешно отправлены!")
-        else:
-            print(f"Ошибка при отправке данных: {response.status_code} - {response.text}")
-
+        try:
+            data = {"name": "VPN", "trigger": "DOS-attack"}
+            json_data = json.dumps(data)
+            url = "http://10.0.2.7:5000/sendnotificate"
+            response = requests.post(url, data=json_data, headers={"Content-Type": "application/json"})
+        except:
+            print("Error send")
+    def SendRegularNotification(self, ip, assumption):
+        try:
+            data = {"name": "VPN"}
+            json_data = json.dumps(data)
+            url = "http://10.0.2.7:5000/RegularNotification"
+            response = requests.post(url, data=json_data, headers={"Content-Type": "application/json"})
+        except:
+            print("Error send")
     def start(self):
         while True:
-            self.ReadingFile()
-            unicationIP = set([elem["id.orig_h"] for elem in self.json_list if elem['id.orig_h'] not in self.itMachineIp])
-            for elem in unicationIP:
-                self.UpploadMachine(elem, [item for item in self.json_list if item["id.orig_h"] == elem])
-
-            for elem in self.machineList:
-                if elem.MachineIsNotActive():
-                    self.machineList.remove(elem)
-            for elem in self.machineList:
-                if elem.IsMachineAttacking() > 0.9:
-                    self.sendmessage(elem.GetIp(), "DoS-атака")
-            for elem in self.machineList:
-                if elem.IsMachineScanning():
-                    self.sendmessage(elem.GetIp(), "Сканирование портов")
-            print("----")
+            try:
+                self.ReadingFile()
+                unicationIP = set([elem["id.orig_h"] for elem in self.json_list if elem['id.orig_h'] not in self.itMachineIp])
+                for elem in unicationIP:
+                    self.UpploadMachine(elem, [item for item in self.json_list if item["id.orig_h"] == elem])
+    
+                for elem in self.machineList:
+                    if elem.MachineIsNotActive():
+                        self.machineList.remove(elem)
+                for elem in self.machineList:
+                    if elem.IsMachineAttacking() > 0.9:
+                        self.sendmessage(elem.GetIp(), "DoS-атака")
+                for elem in self.machineList:
+                    if elem.IsMachineScanning():
+                        self.sendmessage(elem.GetIp(), "Сканирование портов")
+                print("----")
+            except: 
+                print("error start")
             time.sleep(10)
 
 
 
     def UpploadMachine(self, ip, input_traffic):
-        index = self.GetIdMachineForList(ip)
-        if index == -1:            
-            self.machineList.append(
-                machine.Machine(ip, input_traffic, False if len(self.machineList) < self.countMachineKnown else True))
-        else:
-            self.machineList[index].AddTraffic(input_traffic)
-    
+        try:
+            index = self.GetIdMachineForList(ip)
+            if index == -1:            
+                self.machineList.append(
+                    machine.Machine(ip, input_traffic, False if len(self.machineList) < self.countMachineKnown else True))
+            else:
+                self.machineList[index].AddTraffic(input_traffic)
+         except: 
+                print("error UpploadMachine")
     def GetIdMachineForList(self,ip):
         i = 0
         for elem in self.machineList:
@@ -74,52 +75,18 @@ class ManagerForDosDetection():
         return -1
     
     def ReadingFile(self):# считывание с файла, с ранее указаной позиции
-        with open('/opt/zeek/logs/current/conn.log', 'r') as file:
-            file.seek(self.current_position)
-            self.json_list = file.readlines()
-            self.current_position = file.tell()
-            i = 0
-            while i < len(self.json_list):
-                self.json_list[i] = eval(self.json_list[i].replace("true","True").replace("false","False"))
-                i+=1
+        try:
+            with open('/opt/zeek/logs/current/conn.log', 'r') as file:
+                file.seek(self.current_position)
+                self.json_list = file.readlines()
+                self.current_position = file.tell()
+                i = 0
+                while i < len(self.json_list):
+                    self.json_list[i] = eval(self.json_list[i].replace("true","True").replace("false","False"))
+                    i+=1
+        except:
+            print("error ReadingFile")
 
-
-    # #def newIpAddresses():
-    #     def plusCountIpCoef():
-    #         if countIpCoef != 1:
-    #             countIpCoef+=0.1
-    #     def minusCountIpCoef():
-    #         if countIpCoef != 0:
-    #             countIpCoef-=0.1
-    #     def resetCountIpCoef():
-    #         if countIpCoef != 0:
-    #             countIpCoef-=0.1
-    #     unicationIP = set([elem["id.orig_h"] for elem in json_list])
-    #     #добавляем количество ip в список
-    #     if len(countIpList) < 10:
-    #         countIpList.append(len(unicationIP))
-    #     else:
-    #         countIpList.pop(0)
-    #         countIpList.append(len(unicationIP))
-        
-        # # Анализируем количество адресов
-        # # проверка на максимальное количество подлючений
-        # if max(countIpList) == countMachineKnown:
-        #     resetCountIpCoef()
-        #     return
-        #
-        # # если количетсво ip адресов одинакоевое
-        # if len(set(countIpList)) == 1:
-        #     resetCountIpCoef()
-        #     return
-        # # Если количетсво ip адресов варируется
-        # i = 0
-        # while i!=9:
-        #     if countIpList[i] < countIpList[i+1]:
-        #         plusCountIpCoef()
-        #     if countIpList[i] > countIpList[i+1]:
-        #         minusCountIpCoef()
-        # return
 
 manager = ManagerForDosDetection(8)
 manager.start()
